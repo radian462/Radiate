266473eb452e857b9f8226eee1260fab7c03d8ec
radian462 <102207047+radian462@users.noreply.github.com>
2023-11-17 15:42:06 +0900

    /function_list追加

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ tree = app_commands.CommandTree(client)
 async def on_ready():
     print('起動しました')
     await tree.sync()
-
+    
+#機能リスト
+@tree.command(name='function_list', description='機能の一覧を表示します')
+async def function_list(interaction: discord.Interaction):
+  embed = discord.Embed(title="機能一覧",description="・AIチャット\n・DeepL翻訳\n・WolframAlpha計算知能")
+  await interaction.response.send_message(embed=embed)
+  
 #helpコマンド
 @tree.command(name='help', description='ヘルプを表示します')
 async def help(interaction: discord.Interaction):
     embed = discord.Embed(title="ヘルプ")
-    embed.add_field(name="AIチャット",value="`@radianのbot [話す内容]`で会話できます\nELYZA-japanese-Llama-2-7bを搭載しています\n")
-    embed.add_field(name="DeepL翻訳",value="`/deepl [text] [lang] [lang2(省略可)]`で翻訳できます。\ntext:翻訳する文\nlang、lang2:翻訳する言語(選択肢の数の都合上分けました)\n※lang2の言語を使う場合はlangを「lang2の言語を使う」にしてください。\n対応言語:日本語, 英語(イギリス,アメリカ), 中国語(簡字体), 韓国語, ブルガリア語, チェコ語, デンマーク語, ドイツ語, ギリシャ語, エストニア語, スペイン語, フィンランド語, フランス語, ハンガリー語, インドネシア語, イタリア語, リトアニア語, ラトビア語, ノルウェー語(ブークモール), オランダ語, ポーランド語, ポルトガル語(ブラジル,ブラジル以外), ルーマニア語, ロシア語, スロバキア語, スロベニア語, スウェーデン語, トルコ語, ウクライナ語")
-    embed.add_field(name="WolfarmAlpha",value="WolfarmAlphaで計算ができます。\n`/wolfarmalpha [formula]`で計算できます。xの2乗はx^2と書けば出来ます。")
+    embed.add_field(name="・AIチャット",value="`@radianのbot [話す内容]`で会話できます\nELYZA-japanese-Llama-2-7bを搭載しています\n")
+    embed.add_field(name="・DeepL翻訳",value="`/deepl [text] [lang] [lang2(省略可)]`で翻訳できます。\ntext:翻訳する文\nlang、lang2:翻訳する言語(選択肢の数の都合上分けました)\n※lang2の言語を使う場合はlangを「lang2の言語を使う」にしてください。\n対応言語:日本語, 英語(イギリス,アメリカ), 中国語(簡字体), 韓国語, ブルガリア語, チェコ語, デンマーク語, ドイツ語, ギリシャ語, エストニア語, スペイン語, フィンランド語, フランス語, ハンガリー語, インドネシア語, イタリア語, リトアニア語, ラトビア語, ノルウェー語(ブークモール), オランダ語, ポーランド語, ポルトガル語(ブラジル,ブラジル以外), ルーマニア語, ロシア語, スロバキア語, スロベニア語, スウェーデン語, トルコ語, ウクライナ語")
+    embed.add_field(name="・WolfarmAlpha計算知能",value="WolfarmAlpha計算知能で計算ができます。方程式も可能です。\n`/wolfarmalpha [formula]`で計算できます。xの2乗はx^2と書けば出来ます。")
     await interaction.response.send_message(embed=embed)
 
 #Llama2チャット
@@ -83,7 +89,7 @@ async def deepl(interaction: discord.Interaction,text:str,lang:str,lang2:str=Non
   await interaction.response.send_message(embed=embed)
 
 #Wolfalphaコマンド
-@tree.command(name='wolfarmalpha', description='WolfarmAlphaで計算します') 
+@tree.command(name='wolfarmalpha', description='WolfarmAlpha計算知能で計算します') 
 async def wolfarmalpha(interaction: discord.Interaction,formula:str):
   await interaction.response.send_message(WolfarmAlpha.calc(formula))
   
