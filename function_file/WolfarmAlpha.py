942880c0eac4b15e64d63fc97f84e3a0098229e9
radian462 <102207047+radian462@users.noreply.github.com>
2023-12-04 13:35:57 +0900

    整理の影響を解消

--- function_file/WolfarmAlpha.py
+++ function_file/WolfarmAlpha.py
@@ -6,13 +6,13 @@ client = wolframalpha.Client(os.getenv("wolframalpha_api"))
 def calc(request): 
   global text
   res = client.query(request)
-  os.remove('WolframAlpha.txt')
+  os.remove('function_file/WolframAlpha.txt')
   for pod in res.pods:
     for sub in pod.subpods:
       if sub.plaintext:
-        with open("WolframAlpha.txt", "a") as o:
+        with open("function_file/WolframAlpha.txt", "a") as o:
           print(sub.plaintext, file=o)
-        with open("WolframAlpha.txt", 'r') as o:
+        with open("function_file/WolframAlpha.txt", 'r') as o:
           text = o.read()
   
   return(text)
