ca0e2b41dfc3c0baf08fd96c6d85cf55dd3183d4
radian462 <102207047+radian462@users.noreply.github.com>
2023-12-04 13:32:13 +0900

    Delete Translate.py

--- Translate.py
+++
@@ -1,8 +0,0 @@
-import deepl
-import os
-
-API_KEY = os.getenv('DeepL_API')
-
-def deepltranslate(text,lang):
-  translator = deepl.Translator(API_KEY)
-  return(translator.translate_text(text, target_lang=lang))
