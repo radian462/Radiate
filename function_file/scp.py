bc4190ead7c8170121ea5877f32b2721df7f2480
radian462 <102207047+radian462@users.noreply.github.com>
2023-12-21 13:50:17 +0900

    SCPコマンドリメイク

--- function_file/scp.py
+++ function_file/scp.py
@@ -1,58 +1,63 @@
 import requests
 import random
 import re
+import json
+import os
 
-def change_itemnumber(number):
-  if number <= 99 and number > 10:
-    number = f"0{number}"
-  elif number < 10:
-    number = f"00{number}"
-  return number
+def make_database():
+  with open("function_file/scp_date.json", "r+") as f:
+    f.truncate(0)
 
-def get_scpdate():
-  scp_codes = ["", "-EX", "-J", "-ARC", "-JP", "-JP-J", "-JP-EX"]
-  weight_list = [8, 0.5, 0.5, 0.5, 5, 0.5, 0.5]
+  def filter_lines(input_text):
+      lines = input_text.split('\n')
+      filtered_lines = [line for line in lines if re.search(r'<li><a href="/scp-', line) and "scp-ex" not in line or re.search(r'<li><a href="/scpaaaaaaaaaaaaaaaaaa-jp-j', line) or re.search(r'<span class="taboo">', line)]
+      filtered_lines = [re.sub('<span class="taboo">', "", line) for line in filtered_lines]
+      result_text = '\n'.join(filtered_lines)
+      return result_text
 
-  while True:
-      number = random.randint(1, 7999)
-      blueskyblue = random.randint(1, 8900)
-      chosen_code = random.choices(scp_codes, weights=weight_list)[0]
-      url = f"http://scp-jp.wikidot.com/scp-{number}{chosen_code}"
+  url_list = ["scp-series","scp-series-2","scp-series-3","scp-series-4","scp-series-5","scp-series-6","scp-series-7","scp-series-8",
+              "joke-scps","archived-scps","scp-ex","scp-series-jp","scp-series-jp-2","scp-series-jp-3","scp-series-jp-4",
+              "joke-scps-jp","archived-scps-jp","scp-jp-ex"]
 
-      if blueskyblue == 8900:#青い青い空を例外指定
-          number = blueskyblue
+  scp_data = {}
 
-      if requests.get(url).status_code != 404:
-          return number, chosen_code, url
+  for url in url_list:
+      response = requests.get(f"http://scp-jp.wikidot.com/{url}")
+      scp_dates = '\n'.join(re.findall(r'<ul>(.*?)</ul>', response.text, re.DOTALL))
+      filtered_text = filter_lines(scp_dates)
 
-def get_metatitle(itemnumber,code):
-  #オブジェクトクラス取得先決定
-  if code == "" or code == "-JP":
-    if itemnumber < 1000:
-      url = f"http://scp-jp.wikidot.com/scp-series{code}"
-    else:
-      tipnumber = itemnumber//1000 + 1
-      url = f"http://scp-jp.wikidot.com/scp-series{code}-{tipnumber}"  
-  elif code == "-EX" or code == "-JP-EX":
-    url = f"http://scp-jp.wikidot.com/scp{code}"
-  elif code == "-J":
-    url = "http://scp-jp.wikidot.com/joke-scps"
-  elif code == "-ARC":
-    url = "http://scp-jp.wikidot.com/archived-scps"
-  elif code == "-JP-J":
-    url = "http://scp-jp.wikidot.com/joke-scps-jp"
+      with open("function_file/scp_date.html", "w", encoding="utf-8") as f:
+          f.write(filtered_text)
 
-  minicode = code.lower()
-  itemnumber = change_itemnumber(itemnumber)
+      with open("function_file/scp_date.html", encoding="utf-8") as f:
+          for line in f:
+              if '<a class="newpage"' not in line:
+                  scp_id = re.search(r'<li><a href="/(.+?)">', line)
+                  if not re.match(r'scp-\d+', scp_id.group(1)):
+                      scp_id = re.search(fr'<a href="/{scp_id.group(1)}">(.+?)</a>', line)
 
-  response = requests.get(url)
-  match = re.search(fr'<li><a href="/scp-{itemnumber}{minicode}">SCP-{itemnumber}{code}</a>(.+)</li>',response.text)
-  if match:
-    metatitle = match.group(1)
-    return metatitle
+                  itemnumber = re.search(fr'">(.+?)</a>', line)
+                  metatitle = re.search(r'</a>(.+)</li>', line)
+                  if scp_id and itemnumber and metatitle:
+                      scp_data[scp_id.group(1)] = [itemnumber.group(1), metatitle.group(1)]
 
-def randomscp():
-  scpdate = get_scpdate()
-  itemnumber = change_itemnumber(scpdate[0])
-  return f"SCP-{itemnumber}{scpdate[1]}{get_metatitle(scpdate[0],scpdate[1])}\n{(scpdate[2])}"
+      os.remove('function_file/scp_date.html')
 
+  with open("function_file/scp_date.json", mode="w", encoding="utf-8") as f:
+      json.dump(scp_data, f, ensure_ascii=False, indent=2)
+  print("SCPデータを収集しました")
+
+def random_scp():
+  with open('function_file/scp_date.json', 'r') as file:
+    scp_database = json.load(file)
+    scp_id = random.choice(list(scp_database.keys()))
+    itemnumber = scp_database[scp_id][0]
+    metatitle = scp_database[scp_id][1]
+
+    metatitle = re.sub(r'&quot;', '"', metatitle)
+    metatitle = re.sub(r'<strong>|</strong>', '**', metatitle)
+    metatitle = re.sub(r'<span class="rt">(.*?)</span>', r'(\1)', metatitle)
+    metatitle = re.sub(r'<span style="text-decoration: line-through;">(.*?)</span>', r'~~\1~~', metatitle)
+    metatitle = re.sub(r'<(.*?)>', '', metatitle)
+
+  return f"{itemnumber}{metatitle}\nhttp://scp-jp.wikidot.com/{scp_id}"
